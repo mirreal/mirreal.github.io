3a2f2f65dc17327a6e93a64d42f836770af5a45c
mirreal <zgkm181142521@gmail.com>
2024-04-14 22:17:29 +0800

    update seo tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Next.js Blog Starter Kit",
-    description: "Clone and deploy your own Next.js portfolio in minutes.",
-    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    title: "Mirreal.net",
+    description: "Mirreal's note website"
   };
 
   return (
@@ -12,15 +11,14 @@ export default function Document() {
       <Head>
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
+
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+
+        <meta name="twitter:site" content="@zgkmranyu" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
         <Main />
